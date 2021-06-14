dc91f6abcf64b1de1c9da8bac7ed5b2a70622d60
Andrii Sheliah <andriisheliah@gmail.com>
2021-06-14 14:14:11 +0000

    Added some testing sudo check

--- netstat_script/netstat.sh
+++ netstat_script/netstat.sh
@@ -23,5 +23,21 @@ echo "Please type PID or Program name"
 read -p "Please enter a PID or Program name:" type
 echo " You have entered $type"
 
-IP=$(sudo netstat -tunapl | awk '/'$type'/ {print $5}' | cut -d: -f1 | sort | uniq -c | sort | tail -n5 | grep -oP '(\d+\.){3}\d+' | while read IP ; do whois $IP | awk -F':' '/^Organization/ {print $2}' ; done)
-echo $IP
+read -p " You must be root to run netstat properly. Do you want to run netstat with sudo (yes/no?) default - (yes): " become_root
+
+if become_root=yes
+    then echo "Running command as root, please enter your sudo password"
+    sudo netstat -tulnap
+else
+    echo " You must be root to run netstat properly"
+    exit 1
+fi
+#echo "Please make your choice: become root (yes) or run as default user (no))"
+#read become_root
+
+
+
+
+
+#IP=$(netstat -tunapl | awk '/'$type'/ {print $5}' | cut -d: -f1 | sort | uniq -c | sort | tail -n5 | grep -oP '(\d+\.){3}\d+' | while read IP ; do whois $IP | awk -F':' '/^Organization/ {print $2}' ; done)
+#echo $IP
